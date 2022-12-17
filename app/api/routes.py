0c338876b0574154a753c806f1fe98f174f3dda5
Ulises Cota <ulisescota55@gmail.com>
2022-12-17 14:26:19 -0800

    final code for app

---
+++ app/api/routes.py
@@ -0,0 +1,60 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Whiskey, whiskey_schema, whiskeys_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/whiskeys', methods = ['POST'])
+@token_required
+def create_whiskey(current_user_token):
+    name = request.json['name']
+    type_of_whiskey = request.json['type_of_whiskey']
+    price = request.json['price']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    whiskey = Whiskey(name, type_of_whiskey, price, user_token = user_token )
+
+    db.session.add(whiskey)
+    db.session.commit()
+
+    response = whiskey_schema.dump(whiskey)
+    return jsonify(response)
+
+@api.route('/whiskeys', methods = ['GET'])
+@token_required
+def get_whiskey(current_user_token):
+    a_user = current_user_token.token
+    whiskeys = Whiskey.query.filter_by(user_token = a_user).all()
+    response = whiskeys_schema.dump(whiskeys)
+    return jsonify(response)
+
+@api.route('/whiskeys/<id>', methods = ['GET'])
+@token_required
+def get_single_whiskey(current_user_token, id):
+    whiskey = Whiskey.query.get(id)
+    response = whiskey_schema.dump(whiskey)
+    return jsonify(response)
+
+@api.route('/whiskeys/<id>', methods = ['POST','PUT'])
+@token_required
+def update_whiskey(current_user_token,id):
+    whiskey = Whiskey.query.get(id) 
+    whiskey.name = request.json['name']
+    whiskey.type_of_whiskey = request.json['type_of_whiskey']
+    whiskey.price = request.json['price']
+    whiskey.user_token = current_user_token.token
+
+    db.session.commit()
+    response = whiskey_schema.dump(whiskey)
+    return jsonify(response)
+
+@api.route('/whiskeys/<id>', methods = ['DELETE'])
+@token_required
+def delete_whiskey(current_user_token, id):
+    whiskey = Whiskey.query.get(id)
+    db.session.delete(whiskey)
+    db.session.commit()
+    response = whiskey_schema.dump(whiskey)
+    return jsonify(response)
